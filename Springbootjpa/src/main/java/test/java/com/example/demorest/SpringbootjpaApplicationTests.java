ffe6f51207af5c549ca8fd0c88991ccc65d05531
ksvikas424 <srinivasvikask@gmail.com>
2021-04-29 12:48:04 +0530

    Initial Commit

---
+++ Springbootjpa/src/main/java/test/java/com/example/demorest/SpringbootjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.example.demorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringbootjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
